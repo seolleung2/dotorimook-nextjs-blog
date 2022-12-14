c62b7a21c398f5681072e07ec8c21614c2844203
seolleung2 <seolleung22@gmail.com>
2022-12-15 00:42:38 +0900

    add: headlessui, heroicons 설치, 폰트 설정, 네비게이션 바 작업 진행중

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Catamaran: ["Catamaran", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
