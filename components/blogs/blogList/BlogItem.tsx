ff86d5ee1b5fab5683c1f32e97ec8f94765f772c
seolleung2 <seolleung22@gmail.com>
2022-12-28 17:24:30 +0900

    fix: Link 컴포넌트 css 수정 - 화면이 작아질때 블로그 제목 크기에 따라 블로그 아이템 영역의 width 가 고정되는 현상 수정

--- components/blogs/blogList/BlogItem.tsx
+++ components/blogs/blogList/BlogItem.tsx
@@ -13,35 +13,36 @@ const BlogItem: FunctionComponent<Props> = ({ blog }) => {
   const date = fullDate.getDate();
 
   return (
-    <Link href={`/blogs/${blog.slug}`}>
-      <div className="mb-20 flex max-h-96 w-full cursor-pointer flex-col md:max-h-[484px] md:w-[355px] xl:w-[400px] 4xl:min-h-[484px] 4xl:w-[445px]">
-        <div className="relative mb-4 h-56 w-full shadow-xl sm:h-64">
-          <Image
-            src={blog.coverImage}
-            className="rounded-lg object-cover object-center opacity-100 hover:opacity-80"
-            fill
-            alt="blog-thumbnail"
-          />
-          <div className="absolute top-5 left-5 flex h-16 w-16 flex-col items-center justify-center rounded-md bg-black/60 text-2xl leading-none text-white">
-            <span className="font-bold">{date}</span>
-            <span className="mt-1 text-xs">{monthNames[monthIndex]}</span>
-          </div>
+    <Link
+      href={`/blogs/${blog.slug}`}
+      className="mb-20 flex max-h-96 w-full cursor-pointer flex-col md:max-h-[484px] md:w-[355px] xl:w-[400px] 4xl:min-h-[484px] 4xl:w-[445px]"
+    >
+      <div className="relative mb-4 h-56 w-full shadow-xl sm:h-64">
+        <Image
+          src={blog.coverImage}
+          className="rounded-lg object-cover object-center opacity-100 hover:opacity-80"
+          fill
+          alt="blog-thumbnail"
+        />
+        <div className="absolute top-5 left-5 flex h-16 w-16 flex-col items-center justify-center rounded-md bg-black/60 text-2xl leading-none text-white">
+          <span className="font-bold">{date}</span>
+          <span className="mt-1 text-xs">{monthNames[monthIndex]}</span>
         </div>
-        <div className="">
-          <div className="mb-3 flex flex-wrap items-center space-x-4 text-sm font-normal text-[#D10068]">
-            {(blog.categories || []).map((category) => (
-              <span className="block" key={category}>
-                #{category}
-              </span>
-            ))}
-          </div>
-          <h3 className="mb-2 h-[54px] text-lg font-bold leading-normal text-[#292929] line-clamp-2">
-            {blog.title}
-          </h3>
-          <p className="mb-0 max-h-20 text-sm font-normal leading-relaxed text-[#292929] line-clamp-3">
-            {blog.description}
-          </p>
+      </div>
+      <div className="w-full">
+        <div className="mb-3 flex flex-wrap items-center space-x-4 text-sm font-normal text-[#D10068]">
+          {(blog.categories || []).map((category) => (
+            <span className="block" key={category}>
+              #{category}
+            </span>
+          ))}
         </div>
+        <h3 className="mb-2 h-[54px] w-full text-lg font-bold leading-normal text-[#292929] line-clamp-2">
+          {blog.title}
+        </h3>
+        <p className="mb-0 max-h-20 text-sm font-normal leading-relaxed text-[#292929] line-clamp-3">
+          {blog.description}
+        </p>
       </div>
     </Link>
   );
